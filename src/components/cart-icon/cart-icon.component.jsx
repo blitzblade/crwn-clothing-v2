c0fce0e0e6614f684d2ef71473163b2f7943fdb8
Kwesi Dadson <samson.dadson@gmail.com>
2023-08-24 11:45:31 +0000

    stuff added

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,16 @@ import './cart-icon.styles.scss';
 import { CartContext } from '../../contexts/cart.context';
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+    //const totalCart = cartItems.reduce((totalOfItems, cartItem) => totalOfItems + cartItem.quantity , 0)
 
     return (
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
 
         </div>
     )
